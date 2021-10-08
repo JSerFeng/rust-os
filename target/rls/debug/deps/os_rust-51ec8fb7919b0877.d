5e0fa895595751220577ee08e241bfb6ea6eb14d
Fy <1114550440@qq.com>
2021-10-08 19:45:36 +0800

    Delete target directory

--- target/rls/debug/deps/os_rust-51ec8fb7919b0877.d
+++
@@ -1,5 +0,0 @@
-d:\PROJECT\learn-rust\os-rust\target\rls\debug\deps\os_rust-51ec8fb7919b0877.rmeta: src\main.rs
-
-d:\PROJECT\learn-rust\os-rust\target\rls\debug\deps\os_rust-51ec8fb7919b0877.d: src\main.rs
-
-src\main.rs:
--- target/rls/debug/deps/os_rust-e6d5e2632d2bc323.d
+++
@@ -1,5 +0,0 @@
-d:\PROJECT\learn-rust\os-rust\target\rls\debug\deps\os_rust-e6d5e2632d2bc323.rmeta: src\main.rs
-
-d:\PROJECT\learn-rust\os-rust\target\rls\debug\deps\os_rust-e6d5e2632d2bc323.d: src\main.rs
-
-src\main.rs:
